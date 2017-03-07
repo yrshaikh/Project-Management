76925598d8ddaf91a21adabf305ae1afa3bb4b6c
Ali Rizvi <ali.ri@media.net>
2017-03-07 12:02:31 +0530

    added less

--- App/App_Start/BundleConfig.cs
+++ App/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace App
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new LessBundle("~/Content/less").Include(
+                      "~/Content/less/site.less"));
         }
     }
 }
